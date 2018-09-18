22aacc80add014fb44730cc1df537d0966603beb
Danilo Siqueira <danilosic95@gmail.com>
2018-09-18 20:02:32 +0000

    initial commit

---
+++ almoxarifado.js
@@ -0,0 +1,17 @@
+// Almoxarifado.js - Almoxarifado route module
+
+var express = require('express')
+var router = express.Router()
+
+// Home page route
+router.get('/', function (req, res) {
+  res.send('Almoxarifado home page')
+})
+
+// About page route
+router.get('/about', function (req, res) {
+  res.send('About this Almoxarifado')
+})
+
+
+module.exports = router
